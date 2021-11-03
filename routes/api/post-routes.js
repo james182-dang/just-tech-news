44b8b3244519ac84caf848ae97a15014bb831f13
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-11-03 18:19:36 -0500

    bcs push

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Post, User } = require('../../models');
 
-// get all users
+// get all posts
 router.get('/', (req, res) => {
     console.log('===============');
     Post.findAll({
@@ -14,6 +14,73 @@ router.get('/', (req, res) => {
         ]
     })
       .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
+// get specific post by id
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+      .then(dbPostData => {
+          if (!dbPostData) {
+              res.status(404).json({ message: 'No post found with this id' });
+              return;
+          }
+          res.json(dbPostData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
+// create a post
+router.post('/', (req, res) => {
+    // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+    Post.create({
+        title: req.body.title,
+        post_url: req.body.post_url,
+        user_id: req.body.user_id
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
+// Update a post
+router.put('/', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+      .then(dbPostData => {
+          if (!dbPostData) {
+              res.status(404).json({ message: 'No post found with this id' });
+              return;
+          }
+          res.json(dbPostData);
+      })
       .catch(err => {
           console.log(err);
           res.status(500).json(err);
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
-const postRoutes = require('./post-routes');
+
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
 
 router.use((req, res) => {
     res.status(404).end();

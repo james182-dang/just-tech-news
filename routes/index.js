7339120c2acd870f8a26626bc002b160f5fe083e
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-11-03 17:28:53 -0500

    push for BCS

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 router.use((req, res) => {
     res.status(404).end();

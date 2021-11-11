6416813c05e347398839ff4a914902f36873ea0f
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-11-10 18:15:37 -0600

    add dynamic login/logout button, dynamic comment/upvote buttons and functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,8 +30,59 @@ router.get('/', (req, res) => {
     })
       .then(dbPostData => {
           const posts = dbPostData.map(post => post.get({ plain: true }));
-          // pass a single post object into the homoepage template
-          res.render('homepage', { posts });
+          // pass a single post object into the homepage template
+          res.render('homepage', {
+              posts,
+              loggedIn: req.session.loggedIn
+          });
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
+
+router.get('/post/:id', (req, res) => {
+    console.log(req.session);
+
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+      .then(dbPostIdData => {
+          if (!dbPostIdData) {
+              res.status(404).json({ message: 'No post found with this id' });
+              return;
+          }
+
+          const post = dbPostIdData.get({ plain: true });
+          res.render('single-post', {
+              post,
+              loggedIn: req.session.loggedIn
+          });
       })
       .catch(err => {
           console.log(err);
@@ -47,5 +98,6 @@ router.get('/login', (req, res) => {
 
     res.render('login');
 });
+
 
 module.exports = router;
